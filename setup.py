6fc96dde70004cbcb0c9fc3bc52ac9f1874083b0
xingchensong <xingchensong1996@163.com>
2024-09-10 20:22:59 +0800

    Release 0.0.2

--- setup.py
+++ setup.py
@@ -9,8 +9,7 @@ def parse_requirements(filename):
 
 setup(
     name="s3tokenizer",
-    py_modules=["s3tokenizer"],
-    version="0.0.1",
+    version="0.0.2",
     description="Reverse Engineering of Supervised Semantic Speech Tokenizer (S3Tokenizer) proposed in CosyVoice",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
